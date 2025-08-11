5612c260837ecf2707de33c8cf501ec9816c51ce
Sumeya472 <sumeyaseid703@gmail.com>
2025-08-11 17:58:34 +0300

    Create counting sort.py

---
+++ counting sort.py
@@ -0,0 +1,12 @@
+def countingSort(arr):
+    freq = [0] * 100
+    for num in arr:
+        freq[num] += 1
+    return freq
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    arr = list(map(int, input().rstrip().split()))
+    result = countingSort(arr)
+    print(*result)
+
